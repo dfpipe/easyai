c9e21928f31e2919578ecb6a00d6b1c4f79a9ae6
dfpipe <github@dfpipe.com>
2025-01-02 15:16:04 -0800

    Provide free easy ai models

---
+++ dfpipe_eazyai.py
@@ -0,0 +1,38 @@
+import requests
+import os
+
+class HttpsCall:
+
+    @staticmethod
+    def call_with_json_body(url, body):
+        headers = {
+            "Content-Type": "application/json"  # Important for JSON payloads
+        }
+        response = requests.post(url, json=body, headers=headers)
+        if response.status_code != 200:
+            raise Exception(f"Error calling {url}: {response.status_code} {response.text}", response)
+        return response.json()
+
+class EazyaiCustomerReviewModel:
+    url = f"https://api.dfpipe.com/eazyai"
+
+    def __init__(self, apikey=None):
+        self.apikey = apikey
+        if self.apikey is None:
+            self.apikey = os.getenv('DFPIPE_API_KEY')
+        if self.apikey is None:
+            self.apikey = "dfpipe-eazymodel-jikhoh-jybmac-pYxsi9"
+
+    def customer_review_model_v1(self, text_items: list[str]):
+        if len(text_items) >= 20:
+            raise ValueError("Too many items, maximum 20")
+            
+        body = {
+            "apikey": self.apikey,
+            "data": [{"text": text} for text in text_items],
+            "action": "infer",
+            "model_id": "customer_review_sentiment_v1"
+        }
+        body = HttpsCall.call_with_json_body(self.url,body)
+        return body
+
---
+++ stapp.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+if __name__ == "__main__":
+    st.title("Hello world! Welcome to use EazyAI Free model apis")
+
+
+
+
